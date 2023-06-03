77d78b3bd3795c6768e1d33ecfbf11fad0cef79f
Ayanda-bolwatife <ayandastephen1000@gmail.com>
2023-06-03 22:45:23 +0100

    initial commit

--- src/Components/DashBoard.jsx
+++ src/Components/DashBoard.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import SideNavBar from './SideNavBar/SideNavBar'
+import SearchBar from './Main/SearchBar'
+import './Dashboard.css'
 
 const DashBoard = () => {
   return (
     <section>
-        <div>
+        <div className='side'>
             <SideNavBar />
+        </div>
+        <div className='main'>
+            <SearchBar />
         </div>
     </section>
   )
--- src/Components/Main/SearchBar.jsx
+++ src/Components/Main/SearchBar.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import { GrNotification } from 'react-icons/gr'
+import { BsSearch } from 'react-icons/bs'
+import './SearchBar.css'
 
 const SearchBar = () => {
   return (
     <section>
         <div className="search">
-            <input type="search" name="search" id="" />
+            <span><BsSearch /></span>
+            <input type="search" name="search" placeholder="Search" />
         </div>
-        .
+        <div className="profile">
+            <span><GrNotification /></span>
+            <div className='inner-pro'>
+                <img src="/images/Ellipse.png" alt="" />
+                <h1>John Doe</h1>
+            </div>
+        </div>
     </section>
   )
 }
