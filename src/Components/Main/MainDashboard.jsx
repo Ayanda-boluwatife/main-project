5f99b0d1e2041089f8e6eec8ae10a8e93095dadf
Ayanda-bolwatife <ayandastephen1000@gmail.com>
2023-06-04 16:19:38 +0100

    updates

--- src/Components/Main/MainDashboard.jsx
+++ src/Components/Main/MainDashboard.jsx
@@ -10,37 +10,37 @@ const MainDashboard = () => {
               <th>Owner image</th>
               <th>Email</th>
               <th>Phone-number</th>
-              <th>Date</th>
+              <th>Address</th>
           </tr>
           <tr>
               <td><img src="/images/1.png" alt="" /> <span>John Doe</span></td>
               <td>johndoe@gmail.com</td>
               <td>091987654321</td>
-              <td>{new Date().toLocaleDateString()}</td>
+              <td>58 Asa-dam Road</td>
           </tr>
           <tr>
               <td><img src="/images/2.png" alt="" /> <span>Sarah Dimmesk</span></td>
               <td>sarahdimmesk@gmail.com</td>
               <td>091987654321</td> 
-              <td>{new Date().toLocaleDateString()}</td>
+              <td>58 Asa-dam Road</td>
           </tr>
           <tr>
               <td><img src="/images/3.png" alt="" /></td>
               <td>johndoe@gmail.com</td>
               <td>091987654321</td>
-              <td>{new Date().toLocaleDateString()}</td>
+              <td>58 Asa-dam Road</td>
           </tr>
           <tr>
               <td><img src="/images/4.png" alt="" /></td>
               <td>johndoe@gmail.com</td>
               <td>091987654321</td>
-              <td>{new Date().toLocaleDateString()}</td>
+              <td>58 Asa-dam Road</td>
           </tr>
           <tr>
               <td><img src="/images/5.png" alt="" /></td>
               <td>johndoe@gmail.com</td>
               <td>091987654321</td>
-              <td>{new Date().toLocaleDateString()}</td>
+              <td>58 Asa-dam Road</td>
           </tr>
         </table>
       </div>
