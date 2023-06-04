f9f3424484f63d95fc85301ae230aae221bbde65
Ayanda-bolwatife <ayandastephen1000@gmail.com>
2023-06-04 16:53:27 +0100

    updates

--- src/Components/DashBoard.jsx
+++ src/Components/DashBoard.jsx
@@ -3,6 +3,7 @@ import SideNavBar from './SideNavBar/SideNavBar'
 import SearchBar from './Main/SearchBar'
 import './Dashboard.css'
 import MainDashboard from './Main/MainDashboard'
+import { BsPlus } from 'react-icons/bs'
 
 const DashBoard = () => {
   return (
@@ -12,6 +13,18 @@ const DashBoard = () => {
         </div>
         <div className='main'>
             <SearchBar />
+            <div className="header">
+              <div className="left">
+                <h2>Owner Information</h2>
+                <p>Add co-founders or partners to the platform</p>
+              </div>
+              <div className="right">
+                <a href="." className="btn">
+                  <BsPlus />
+                  Add Owner
+                </a>
+              </div>
+            </div>
             <MainDashboard />
         </div>
     </section>
--- src/Components/Main/MainDashboard.jsx
+++ src/Components/Main/MainDashboard.jsx
@@ -7,38 +7,38 @@ const MainDashboard = () => {
       <div className="table-head">
         <table>
           <tr>
-              <th>Owner image</th>
+              <th>Owner-Info</th>
               <th>Email</th>
               <th>Phone-number</th>
               <th>Address</th>
           </tr>
           <tr>
-              <td><img src="/images/1.png" alt="" /> <span>John Doe</span></td>
+              <td><div className='user-info'><img src="/images/1.png" alt="" /> <span>John Doe</span></div></td>
               <td>johndoe@gmail.com</td>
               <td>091987654321</td>
               <td>58 Asa-dam Road</td>
           </tr>
           <tr>
-              <td><img src="/images/2.png" alt="" /> <span>Sarah Dimmesk</span></td>
+              <td><div className='user-info'><img src="/images/2.png" alt="" /> <span>Sarah Dimmesk</span></div></td>
               <td>sarahdimmesk@gmail.com</td>
               <td>091987654321</td> 
               <td>58 Asa-dam Road</td>
           </tr>
           <tr>
-              <td><img src="/images/3.png" alt="" /></td>
-              <td>johndoe@gmail.com</td>
+              <td><div className='user-info'><img src="/images/3.png" alt="" /> <span>Gabriel Kotesky</span></div></td>
+              <td>gabriel@gmail.com</td>
               <td>091987654321</td>
               <td>58 Asa-dam Road</td>
           </tr>
           <tr>
-              <td><img src="/images/4.png" alt="" /></td>
-              <td>johndoe@gmail.com</td>
+              <td><div className='user-info'><img src="/images/4.png" alt="" /> <span>Katrina Johnson</span></div></td>
+              <td>katrina@gmail.com</td>
               <td>091987654321</td>
               <td>58 Asa-dam Road</td>
           </tr>
           <tr>
-              <td><img src="/images/5.png" alt="" /></td>
-              <td>johndoe@gmail.com</td>
+              <td><div className='user-info'><img src="/images/5.png" alt="" /> <span>James Berry</span></div></td>
+              <td>james@gmail.com</td>
               <td>091987654321</td>
               <td>58 Asa-dam Road</td>
           </tr>
